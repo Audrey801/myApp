c74191fa3b49a70dc6f8b4dcc9ca60fde923a151
Audrey Pinheiro <audreypinheiro801@gmail.com>
2021-09-23 20:45:03 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bom dia,Audrey!");
+            Console.WriteLine("");
+            Console.WriteLine("O Dia  é "+ DateTime.Now + " horas");
+
+            Console.WriteLine("Está Na Hora de Trabalhar");
+            Console.WriteLine();
+            Console.WriteLine("Até Mais!");
+
+
+            //Console.ReadKey();
+        }
+    }
+}
+
+
